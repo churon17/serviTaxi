8d46976f6cd0e4b2c394d2d3c1f0529aec319565
Jean Carlos Alarcón <jeancarlosalarguez16@gmail.com>
2018-08-18 01:50:39 -0500

    Splash Screen
    Add Spash Screen

--- app/src/main/java/jeancarlosdev/servitaxi/Splash_1.java
+++ app/src/main/java/jeancarlosdev/servitaxi/Splash_1.java
@@ -1,5 +1,7 @@
 package jeancarlosdev.servitaxi;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,18 @@ public class Splash_1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.frm_splash_1);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext()
+                        ,Login_2.class ));
+
+            }
+        }, 4000);
+
+
+
     }
 }
